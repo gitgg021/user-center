bb8217e6589685d1f91e82cd05392db08a11bb80
caoxuehan <caoxuehan@qq.com>
2024-01-27 19:53:46 +0800

    MybatisPlus-UserMapper

---
+++ src/main/java/com/example/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.example.mapper;
+
+import com.example.model.domin.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author caocao
+* @description 针对表【user(用户)】的数据库操作Mapper
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
