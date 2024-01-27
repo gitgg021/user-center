8c013f375a1e2be602506a97019bfcf98c2d6ca7
caoxuehan <caoxuehan@qq.com>
2024-01-27 20:10:17 +0800

    返回工具类

---
+++ src/main/java/com/example/common/ResultUtils.java
@@ -0,0 +1,59 @@
+package com.example.common;
+/**
+ * 返回工具类
+ * @author caocao
+ */
+public class ResultUtils {
+    /**
+     * 成功
+     * @param data
+     * @param <T>
+     * @return
+     */
+    //<T>声明方法为泛型方法 BaseResponse<T>返回值
+    public static <T> BaseResponse<T> success(T data){
+        return new BaseResponse<>(0,data,"ok");
+    }
+
+    /**
+     * 失败
+     * @param errorCode
+     * @return
+     */
+    public static BaseResponse error(ErrorCode errorCode){
+        return new BaseResponse<>(errorCode);
+    }
+
+    /**
+     * 失败
+     * @param code
+     * @param message
+     * @param description
+     * @return
+     */
+    public static BaseResponse error(int code,String message, String description){
+        return new BaseResponse<>(code,null,message,description);
+    }
+
+    /**
+     * 失败
+     * @param errorCode
+     * @param message
+     * @param description
+     * @return
+     */
+    public static BaseResponse error(ErrorCode errorCode,String message, String description){
+        return new BaseResponse(errorCode.getCode(),null,message,description);
+    }
+
+    /**
+     * 失败
+     * @param errorCode
+     * @param description
+     * @return
+     */
+
+    public static BaseResponse error(ErrorCode errorCode,String description){
+        return new BaseResponse(errorCode.getCode(), errorCode.getMessage(),description);
+    }
+}
