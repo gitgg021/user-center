3653b00b1c354b888e9f9bdca430aaef5e9bf4d6
caoxuehan <caoxuehan@qq.com>
2024-01-27 19:36:02 +0800

    项目初始化

---
+++ src/main/java/com/example/model/domin/User.java
@@ -0,0 +1,90 @@
+package com.example.model.domin;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 用户
+ * @TableName user
+ */
+@TableName(value ="user")
+@Data
+public class User implements Serializable {
+    /**
+     * 
+     */
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 账号
+     */
+    private String userAccount;
+
+    /**
+     * 用户昵称
+     */
+    private String username;
+
+    /**
+     * 用户头像
+     */
+    private String avatarUrl;
+
+    /**
+     * 性别
+     */
+    private Integer gender;
+
+    /**
+     * 用户密码
+     */
+    private String userPassword;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 状态 0-正常
+     */
+    private Integer userStatus;
+
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    /**
+     * 是否删除
+     */
+    @TableLogic
+    private Integer isDelete;
+
+    /**
+     * 普通用户-0 管理员-1
+     */
+    private Integer userRole;
+
+    /**
+     * 星球编号
+     */
+    private String planetCode;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
