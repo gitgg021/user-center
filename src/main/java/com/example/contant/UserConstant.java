9b767e0f2cd0140d328314fc6d0b4d0a0b5b3a1d
caoxuehan <caoxuehan@qq.com>
2024-01-28 22:29:40 +0800

    用户登录态

---
+++ src/main/java/com/example/contant/UserConstant.java
@@ -0,0 +1,28 @@
+package com.example.contant;
+
+
+/**
+ * 用户登录
+ *
+ * @Author cao
+ */
+public interface UserConstant {
+    /**
+     * 用户登录态 键
+     */
+    String USER_LOGIN_STATE  = "userLoginState";
+
+     //------权限-------
+
+    /**
+     * 默认权限
+     */
+    int DEFAULT_ROLE = 0;
+    /**
+     * 管理员
+     */
+    int ADMIN_ROLE = 1;
+
+
+
+}
